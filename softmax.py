8c2f6cbd59927a83d9f786e236d4cd13ac2a2f42
Yash Shah <yashshah00@outlook.com>
2026-05-13 13:20:59 +0100

    Separated executable part of `softmax.py`.

--- softmax.py
+++ softmax.py
@@ -152,8 +152,9 @@ def softmax(x):
     return y
 
 
-torch.manual_seed(0)
-x = torch.randn(1823, 781, device=DEVICE)
-y_triton = softmax(x)
-y_torch = torch.softmax(x, axis=1)
-assert torch.allclose(y_triton, y_torch), (y_triton, y_torch)
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    x = torch.randn(1823, 781, device=DEVICE)
+    y_triton = softmax(x)
+    y_torch = torch.softmax(x, axis=1)
+    assert torch.allclose(y_triton, y_torch), (y_triton, y_torch)
